9b880e1d54c9a5e59a7960024ab38c9a49fa88f5
Martin R. Hristov <martin.r.hristov@sap.com>
2016-06-04 13:59:23 +0300

    login added

--- BookMarks/LoginViewController.swift
+++ BookMarks/LoginViewController.swift
@@ -1,46 +1,45 @@
 //
-//  RegisterViewController.swift
+//  LoginViewController.swift
 //  BookMarks
 //
-//  Created by Hristov, Martin R. on 5/29/16.
+//  Created by Hristov, Martin R. on 6/4/16.
 //  Copyright © 2016 Hristov, Martin R. All rights reserved.
 //
 
 import UIKit
 import Firebase
 
-class RegisterViewController: UIViewController {
+class LoginViewController: UIViewController {
 
-    @IBOutlet weak var passField: UITextField!
     @IBOutlet weak var emailField: UITextField!
-    @IBOutlet weak var registerBtn: UIButton!
-
+    @IBOutlet weak var passField: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        registerBtn.addTarget(self, action: #selector(RegisterViewController.registerUser), forControlEvents: UIControlEvents.TouchUpInside)
+        loginBtn.addTarget(self, action: #selector(LoginViewController.loginUser), forControlEvents: UIControlEvents.TouchUpInside)
+        
 
         // Do any additional setup after loading the view.
-    }
-    
-    func registerUser () {
-        let password = passField.text
-        let email = emailField.text
-        let myApp = FIRAuth(app: FIRApp.defaultApp()!)
-        
-        myApp!.createUserWithEmail(email!, password: password!, completion: { (user, error) in
-            if let error = error {
-                print("Sign up failed:", error.localizedDescription)
-            } else {
-                print ("Signed up with uid:", user!.uid)
-                print(myApp!.currentUser)
-            }
-        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loginUser() {
+        let password = passField.text
+        let email = emailField.text
+        let myApp = FIRAuth(app: FIRApp.defaultApp()!)
+        
+        myApp!.signInWithEmail(email!, password: password!, completion: { (user, error) in
+            if let error = error {
+                print("Sign in failed:", error.localizedDescription)
+            } else {
+                print ("Signed in with uid:", user!.uid)
+            }
+        })
     }
     
 
