d3c65fa7408368f2e0070463645241520de0409b
Martin R. Hristov <martin.r.hristov@sap.com>
2016-06-07 00:08:40 +0300

    template for isUserLogged added

--- BookMarks/SearchDetailsViewController.swift
+++ BookMarks/SearchDetailsViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import Firebase
 
 class SearchDetailsViewController: UIViewController {
 
     var book = Book()
     override func viewDidLoad() {
         super.viewDidLoad()
+        let myApp = FIRAuth(app: FIRApp.defaultApp()!)
+        FIRAuth.auth()!.addAuthStateDidChangeListener() { (auth, user) in
+            if let user = user {
+                print("toq e lognat: ", user.uid)
+                try! FIRAuth.auth()!.signOut()
+            } else {
+                print("nqma nikoi lognat")
+            }
+        }
         self.view.backgroundColor = UIColor.whiteColor()
     }
 }
