515fd2672968e421c6f52764d03e242b51eb2f20
Martin R. Hristov <martin.r.hristov@sap.com>
2016-06-07 22:07:40 +0300

    Details page done

--- BookMarks/SearchDetailsViewController.swift
+++ BookMarks/SearchDetailsViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 import Firebase
+import Cosmos
 
 class SearchDetailsViewController: UIViewController {
 
     var book = Book()
     override func viewDidLoad() {
         super.viewDidLoad()
+        let scrollView = UIScrollView(frame: view.bounds)
+        scrollView.contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height)
+        scrollView.scrollEnabled = true
+        self.view.addSubview(scrollView)
         self.view.backgroundColor = UIColor.whiteColor()
         let logoHeight = (self.view.bounds.size.height - 128)/2
         let bookLogo = UIImageView()
@@ -22,13 +27,17 @@ class SearchDetailsViewController: UIViewController {
         let hrLine = UIView()
         let readBtn = UIButton()
         let rateLbl = UILabel()
+        let cosmosView = CosmosView()
+        let fullHrLine = UIView()
+        let descTitle = UILabel()
+        let description = UITextView()
 
         
         bookLogo.image = UIImage(data: book.imgData)
         bookLogo.contentMode = UIViewContentMode.ScaleAspectFit
-        bookLogo.frame = CGRect(x: 0, y: 84, width: self.view.bounds.size.width, height: logoHeight)
-        title.frame = CGRect(x: 0, y: logoHeight + 84, width: self.view.bounds.size.width, height: 44)
-        author.frame = CGRect(x: 0, y: logoHeight + 124, width: self.view.bounds.size.width, height: 28)
+        bookLogo.frame = CGRect(x: 0, y: 20, width: self.view.bounds.size.width, height: logoHeight)
+        title.frame = CGRect(x: 0, y: logoHeight + 30, width: self.view.bounds.size.width, height: 44)
+        author.frame = CGRect(x: 0, y: logoHeight + 64, width: self.view.bounds.size.width, height: 28)
         
         title.text = book.name
         title.font = UIFont(name: title.font.fontName, size: 20)
@@ -40,22 +49,45 @@ class SearchDetailsViewController: UIViewController {
         
         hrLine.layer.borderWidth = 1.0
         hrLine.layer.borderColor = UIColor.blackColor().CGColor
-        hrLine.frame = CGRect(x: 10, y: logoHeight + 160, width: self.view.bounds.size.width - 20, height: 1)
+        hrLine.frame = CGRect(x: 10, y: logoHeight + 100, width: self.view.bounds.size.width - 20, height: 1)
        
-        readBtn.frame = CGRect(x: (self.view.bounds.size.width - 180)/2 , y: logoHeight + 180, width: 180, height: 38)
+        readBtn.frame = CGRect(x: (self.view.bounds.size.width - 180)/2 , y: logoHeight + 120, width: 180, height: 38)
         readBtn.backgroundColor = UIColor.blackColor()
         readBtn.setTitle("Want to Read", forState: UIControlState.Normal)
         
-        rateLbl.frame = CGRect(x: 0 , y: logoHeight + 220, width: self.view.bounds.size.width, height: 38)
+        rateLbl.frame = CGRect(x: 0 , y: logoHeight + 160, width: self.view.bounds.size.width, height: 38)
         rateLbl.text = "Rate this book:"
         rateLbl.textAlignment = NSTextAlignment.Center
         rateLbl.font = UIFont(name: author.font.fontName, size: 10)
+        
+        cosmosView.settings.starSize = 30
+        cosmosView.frame = CGRect(x: (self.view.bounds.size.width/2)-85, y: logoHeight + 200, width: 0, height: 0)
+        
+        fullHrLine.layer.borderWidth = 1.0
+        fullHrLine.layer.borderColor = UIColor.blackColor().CGColor
+        fullHrLine.frame = CGRect(x: 0, y: logoHeight + 250, width: self.view.bounds.size.width, height: 1)
+        
+        descTitle.text = "Book Description"
+        descTitle.font = UIFont(name: author.font.fontName, size: 16)
+        descTitle.textAlignment = NSTextAlignment.Center
+        descTitle.frame = CGRect(x: 0, y: logoHeight + 270, width: self.view.bounds.size.width, height: 20)
+        
+        description.text = book.bookDescription
+        description.font = UIFont(name: author.font.fontName, size: 12)
+        description.frame = CGRect(x: 0, y: logoHeight + 270, width: self.view.bounds.size.width, height: description.contentSize.height)
+        scrollView.contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height + description.contentSize.height - 64)
+        description.frame = CGRect(x: 0, y: logoHeight + 290, width: self.view.bounds.size.width, height: description.contentSize.height)
 
-        self.view.addSubview(bookLogo)
-        self.view.addSubview(title)
-        self.view.addSubview(author)
-        self.view.addSubview(hrLine)
-        self.view.addSubview(readBtn)
-        self.view.addSubview(rateLbl)
+        print(description.contentSize.height)
+        scrollView.addSubview(bookLogo)
+        scrollView.addSubview(title)
+        scrollView.addSubview(author)
+        scrollView.addSubview(hrLine)
+        scrollView.addSubview(readBtn)
+        scrollView.addSubview(rateLbl)
+        scrollView.addSubview(cosmosView)
+        scrollView.addSubview(fullHrLine)
+        scrollView.addSubview(descTitle)
+        scrollView.addSubview(description)
     }
 }
--- Podfile
+++ Podfile
@@ -1,14 +1,14 @@
 # Uncomment this line to define a global platform for your project
 # platform :ios, '8.0'
 # Uncomment this line if you're using Swift
-# use_frameworks!
+use_frameworks!
 
 target 'BookMarks' do
     pod 'Firebase', '>= 2.5.1'
     pod 'Firebase/Core'
     pod 'Firebase/Database'
     pod 'Firebase/Auth'
-
+    pod 'Cosmos', '~> 1.2'
 end
 
 target 'BookMarksTests' do
