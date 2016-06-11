002b0ca7d51c6a68d1337cf47c78f363926661ce
Martin R. Hristov <martin.r.hristov@sap.com>
2016-06-11 14:40:53 +0300

    TabBarItem for my books added

---
+++ BookMarks/MyBookViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MyBookViewController.swift
+//  BookMarks
+//
+//  Created by Hristov, Martin R. on 6/11/16.
+//  Copyright © 2016 Hristov, Martin R. All rights reserved.
+//
+
+import UIKit
+
+class MyBookViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
